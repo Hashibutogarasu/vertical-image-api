127895fca6ad7dfc2d969f4fd021af133e9e3b7f
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2023-09-24 01:18:50 +0900

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
+
 const nextConfig = {
     reactStrictMode: true,
+    assetPrefix: branchName,
+    basePath: branchName,
     swcMinify: true,
     images: {
         unoptimized: true,
     },
 }
+module.exports = nextConfig;
